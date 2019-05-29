c58938a2c8ba3a8efb4263ca50513d6de10aaa20
Vladislav Tsirulev <Vladislav.Tsirulev@columbusglobal.com>
2019-05-29 17:07:23 +0300

    Push

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Azure.NotificationHubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SendNotificationAsync();
+            Console.Write("Test");
+            Console.ReadLine();
+        }
+        private static async void SendNotificationAsync()
+        {
+            NotificationHubClient hub = NotificationHubClient.CreateClientFromConnectionString("Endpoint=sb://pushcrm.servicebus.windows.net/;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=KF47Bbxdhz+nnninLfbIEi/VXPPYVKa5ZaMwkO5I6/U=", "Test_notification_hub");
+            String message = "{\"data\":{\"Test message\":\"Доров\"}}";
+            await hub.SendFcmNativeNotificationAsync(message);
+        }
+    }
+}
